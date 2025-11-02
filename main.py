e52df0e9e81ff8b0baa747d173ec8b713a88e651
Balaji Kanagaraju <balajik@live.com>
2025-11-02 06:24:21 +0000

    Inital commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/api/orders")
+async def create_order(request: Request):
+    data = await request.json()
+    print("📦 New order received:", data)
+    return {
+        "status": "success",
+        "message": "Order received successfully",
+        "data": data
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
